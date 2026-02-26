40d22e646366cf9c7637e655b6b4688015d100c6
Crispertree <patriciapettersson@live.com>
2026-02-26 17:30:35 +0100

    Lagt till home tagit bort flik

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-const bodyElement = document.querySelector("body");
+const inputElement = document.getElementById("search-bar");
+const buttonElement = document.getElementById("search-entry");
+
+buttonElement.addEventListener("click" function (event) {
+    let enteredName = inputElement.value;
+    HeadingElement.innertext = "Search results:" + enteredName;
+
+}
