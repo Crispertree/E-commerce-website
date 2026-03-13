c2d4ea367f886fdeb8be73aeb85dc1b5c53f43cb
Crispertree <patriciapettersson@live.com>
2026-03-13 12:02:11 +0100

    cart

--- js/main.js
+++ js/main.js
@@ -1,9 +1,62 @@
-const inputElement = document.getElementById("search-bar");
-const buttonElement = document.getElementById("search-entry");
+function displayCart() {
 
-buttonElement.addEventListener("click" function (event) {
-    let enteredName = inputElement.value;
-    HeadingElement.innertext = "Search results:" + enteredName;
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+  let container = document.getElementById("cart-items");
+
+  container.innerHTML = "";
+
+  if (cart.length === 0) {
+  container.innerHTML = "<p>Your cart is empty.</p>";
+  return;
+}
+
+  cart.forEach((item, index) => {
+
+    container.innerHTML += `
+      <div>
+        <h3>${item.name}</h3>
+        <p>${item.price} kr</p>
+
+        <button onclick="removeItem(${index})">
+          Remove
+        </button>
+      </div>
+    `;
+  });
+
+}
+
+displayCart();
+
+function removeItem(index) {
+
+  let cart = JSON.parse(localStorage.getItem("cart"));
+
+  cart.splice(index, 1);
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  displayCart();
+}
+function updateCartCount() {
+
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  document.getElementById("cart-count").innerText = cart.length;
+}
+function addToCart(id, name, price) {
+
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  cart.push({
+    id: id,
+    name: name,
+    price: price
+  });
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  alert("Added to cart!");
 }
 
